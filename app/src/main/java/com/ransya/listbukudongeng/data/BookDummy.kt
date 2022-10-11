836c904f086f9270fd8e0edf4fad2e66ebd52dab
Risma Adisty Nilasari <67015218+rismaadisty@users.noreply.github.com>
2022-10-11 19:59:10 +0700

    Feature/develop (#1)
    * add style
    
    * finish feature list book

---
+++ app/src/main/java/com/ransya/listbukudongeng/data/BookDummy.kt
@@ -0,0 +1,43 @@
+package com.ransya.listbukudongeng.data
+
+import com.ransya.listbukudongeng.R
+import com.ransya.listbukudongeng.model.Book
+
+
+val listBook = listOf(
+    Book(
+        noBook = 1,
+        judulBuku = "Nusantara",
+        tahun = "2001",
+        penerbit = "Bandung",
+        image = R.drawable.buku_1
+    ),
+    Book(
+        noBook = 2,
+        judulBuku = "Kancil ",
+        tahun = "2002",
+        penerbit = "Jakarta",
+        image = R.drawable.buku_2
+    ),
+    Book(
+        noBook = 3,
+        judulBuku = "Lutung Kasarung ",
+        tahun = "2003",
+        penerbit = "Surakarta",
+        image = R.drawable.buku_3
+    ),
+    Book(
+        noBook = 4,
+        judulBuku = "Hutan Damai",
+        tahun = "2004",
+        penerbit = "Manado",
+        image = R.drawable.buku_4
+    ),
+    Book(
+        noBook = 5,
+        judulBuku = "Hutan Cahaya",
+        tahun = "2005",
+        penerbit = "Magelang",
+        image = R.drawable.buku_5
+    ),
+)
---
+++ app/src/main/java/com/ransya/listbukudongeng/model/Book.kt
@@ -0,0 +1,9 @@
+package com.ransya.listbukudongeng.model
+
+data class Book(
+    var noBook : Int,
+    var judulBuku : String,
+    var tahun : String,
+    var penerbit : String,
+    var image: Int
+)
